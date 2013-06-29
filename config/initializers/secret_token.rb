b7d05fba06537caf3bb312cdc3c4567ddde17d3a
Rika Nguyen <jerika.nguyen@gmail.com>
2013-06-29 02:23:36 -0700

    in1ti4l c0mm1t

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RikaIo::Application.config.secret_token = '42cbe4a5ff1d96d37a833dd2bf5e4109cb669d1e55a912cc80b707bfb4d6be6de7b986b8aca8407315abbf15ce3410844aa324a35d6a1c1332e28b82672288ec'
